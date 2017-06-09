d1378cf6bd7df34663ffd9bae73b1972f5755164
bdistin <bdistin@gmail.com>
2017-06-08 20:27:52 -0500

    Couple bug fixes/unnested function defines

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const path = require('path');
+const { sep, resolve, dirname, join, normalize, isAbsolute, relative } = require('path');
 const { promisify } = require('util');
 
 const targets = [
@@ -30,18 +30,18 @@ for (const [key, value] of Object.entries(fs)) {
 fsn.copy = async(src, dest, options = {}) => {
 	if (typeof options === 'function') options = { filter: options };
 	const basePath = process.cwd();
-	const currentPath = path.resolve(basePath, src);
-	const targetPath = path.resolve(basePath, dest);
+	const currentPath = resolve(basePath, src);
+	const targetPath = resolve(basePath, dest);
 	if (currentPath === targetPath) throw new Error('Source and destination must not be the same.');
 	const stats = await fsn.lstat(src);
-	const dir = stats.isDirectory() ? dest.split(path.sep).slice(0, -1).join(path.sep) : path.dirname(dest);
+	const dir = stats.isDirectory() ? dest.split(sep).slice(0, -1).join(sep) : dirname(dest);
 	if (!await fsn.pathExists(dir)) await fsn.mkdirs(dir);
 	return ncp(src, dest, options);
 };
 
 fsn.emptyDir = fsn.emptydir = async (dir) => {
 	const items = await fsn.readdir(dir).catch(() => fsn.mkdirs(dir));
-	return Promise.all(items.map(item => fsn.remove(path.join(dir, item))));
+	return Promise.all(items.map(item => fsn.remove(join(dir, item))));
 };
 
 fsn.ensureDir = fsn.mkdirs = fsn.mkdirp = async(myPath, opts, made = null) => {
@@ -53,7 +53,7 @@ fsn.ensureDir = fsn.mkdirs = fsn.mkdirp = async(myPath, opts, made = null) => {
 	}
 	// eslint-disable-next-line no-bitwise
 	const mode = opts.mode || o777 & ~process.umask();
-	myPath = path.resolve(myPath);
+	myPath = resolve(myPath);
 	return fsn.mkdir(myPath, mode)
 		.then(() => made || myPath)
 		.catch((err) => {
@@ -65,15 +65,15 @@ fsn.ensureDir = fsn.mkdirs = fsn.mkdirp = async(myPath, opts, made = null) => {
 					})
 					.catch(() => { throw err; });
 			}
-			if (path.dirname(myPath) === myPath) throw err;
-			return fsn.mkdirs(path.dirname(myPath), opts)
+			if (dirname(myPath) === myPath) throw err;
+			return fsn.mkdirs(dirname(myPath), opts)
 				.then(madeChain => fsn.mkdirs(myPath, opts, madeChain));
 		});
 };
 
 fsn.ensureFile = fsn.createFile = async (file) => {
 	if (await fsn.pathExists(file)) return null;
-	const dir = path.dirname(file);
+	const dir = dirname(file);
 	if (!await fsn.pathExists(dir)) await fsn.mkdirs(dir);
 	return fsn.writeFile(file, '');
 };
@@ -81,17 +81,17 @@ fsn.ensureFile = fsn.createFile = async (file) => {
 fsn.ensureLink = fsn.createLink = async (srcpath, dstpath) => {
 	if (await fsn.pathExists(dstpath)) return null;
 	await fsn.lstat(srcpath).catch(err => { throw err.message.replace('lstat', 'ensureLink'); });
-	const dir = path.dirname(dstpath);
+	const dir = dirname(dstpath);
 	if (!await fsn.pathExists(dir)) await fsn.mkdirs(dir);
 	return fsn.link(srcpath, dstpath);
 };
 
 fsn.ensureSymlink = fsn.createSymlink = async (srcpath, dstpath, type) => {
 	if (await fsn.pathExists(dstpath)) return null;
-	const relative = await symlinkPaths(srcpath, dstpath);
-	srcpath = relative.toDst;
-	const type2 = await symlinkType(relative.toCwd, type);
-	const dir = path.dirname(dstpath);
+	const relativePath = await symlinkPaths(srcpath, dstpath);
+	srcpath = relativePath.toDst;
+	const type2 = await symlinkType(relativePath.toCwd, type);
+	const dir = dirname(dstpath);
 	if (!await fsn.pathExists(dir)) await fsn.mkdirs(dir);
 	return fsn.symlink(srcpath, dstpath, type2);
 };
@@ -100,15 +100,15 @@ fsn.move = async (source, dest, options) => {
 	const shouldMkdirp = 'mkdirp' in options ? options.mkdirp : true;
 	const overwrite = options.overwrite || options.clobber || false;
 
-	if (shouldMkdirp) await fsn.mkdirs(path.dirname(dest)).catch(err => { throw err; });
+	if (shouldMkdirp) await fsn.mkdirs(dirname(dest)).catch(throwErr);
 
-	if (path.resolve(source) === path.resolve(dest)) {
+	if (resolve(source) === resolve(dest)) {
 		return fsn.access(source);
 	} else if (overwrite) {
 		return fsn.rename(source, dest)
 			.catch(async(err) => {
 				if (err.code === 'ENOTEMPTY' || err.code === 'EEXIST') {
-					await fsn.remove(dest).catch(er => { throw er; });
+					await fsn.remove(dest).catch(throwErr);
 					options.overwrite = false;
 					return fsn.move(source, dest, options);
 				}
@@ -116,7 +116,7 @@ fsn.move = async (source, dest, options) => {
 				// Windows
 				if (err.code === 'EPERM') {
 					await setTimeoutPromise(200);
-					await fsn.remove(dest).catch(er => { throw er; });
+					await fsn.remove(dest).catch(throwErr);
 					options.overwrite = false;
 					return fsn.move(source, dest, options);
 				}
@@ -134,14 +134,14 @@ fsn.move = async (source, dest, options) => {
 };
 
 fsn.outputFile = async (file, data, encoding) => {
-	const dir = path.dirname(file);
+	const dir = dirname(file);
 	if (!await fsn.pathExists(dir)) await fsn.mkdirs(dir);
 	return fsn.writeFile(file, data, encoding);
 };
 
 fsn.outputJSON = fsn.outputJson = async (file, data, options) => {
-	const dir = path.dirname(file);
-	if (!await fsn.pathExists(path)) await fsn.mkdirs(dir);
+	const dir = dirname(file);
+	if (!await fsn.pathExists(dir)) await fsn.mkdirs(dir);
 	return fsn.writeJson(file, data, options);
 };
 
@@ -178,13 +178,15 @@ fsn.writeJSON = fsn.writeJson = async (file, obj, options = {}) => {
 	return fsn.writeFile(file, str, options);
 };
 
+const throwErr = err => { throw err; };
+
 const stripBom = (content) => {
 	if (Buffer.isBuffer(content)) content = content.toString('utf8');
 	return content.replace(/^\uFEFF/, '');
 };
 
 const invalidWin32Path = (myPath) => {
-	const root = path.normalize(path.resolve(myPath)).split(path.sep);
+	const root = normalize(resolve(myPath)).split(sep);
 	const rp = root.length > 0 ? root[0] : null;
 	return /[<>:"|?*]/.test(myPath.replace(rp, ''));
 };
@@ -196,24 +198,24 @@ const symlinkType = async (srcpath, type = false) => {
 };
 
 const symlinkPaths = async (srcpath, dstpath) => {
-	if (path.isAbsolute(srcpath)) {
+	if (isAbsolute(srcpath)) {
 		await fsn.lstat(srcpath).catch(err => { throw err.message.replace('lstat', 'ensureSymlink'); });
 		return { toCwd: srcpath, toDst: srcpath };
 	}
-	const dstdir = path.dirname(dstpath);
-	const relativeToDst = path.join(dstdir, srcpath);
+	const dstdir = dirname(dstpath);
+	const relativeToDst = join(dstdir, srcpath);
 	if (await fsn.pathExists(relativeToDst)) return { toCwd: relativeToDst, toDst: srcpath };
 	await fsn.lstat(srcpath).catch(err => { throw err.message.replace('lstat', 'ensureSymlink'); });
-	return { toCwd: srcpath, toDst: path.relative(dstdir, srcpath) };
+	return { toCwd: srcpath, toDst: relative(dstdir, srcpath) };
 };
 
 const moveAcrossDevice = async (source, dest, overwrite) => {
-	const stat = await fsn.stat(source).catch(err => { throw err; });
+	const stat = await fsn.stat(source).catch(throwErr);
 	if (stat.isDirectory()) return moveDirAcrossDevice(source, dest, overwrite);
 	return moveFileAcrossDevice(source, dest, overwrite);
 };
 
-const moveFileAcrossDevice = (source, dest, overwrite) => new Promise((resolve, reject) => {
+const moveFileAcrossDevice = (source, dest, overwrite) => new Promise((res, rej) => {
 	const flags = overwrite ? 'w' : 'wx';
 	const ins = fsn.createReadStream(source);
 	const outs = fsn.createWriteStream(dest, { flags });
@@ -221,29 +223,29 @@ const moveFileAcrossDevice = (source, dest, overwrite) => new Promise((resolve, 
 	ins.on('error', err => {
 		ins.destroy();
 		outs.destroy();
-		outs.removeListener('close', () => { resolve(fsn.unlink(source)); });
+		outs.removeListener('close', () => { res(fsn.unlink(source)); });
 
 		fsn.unlink(dest).catch(() => {
-			if (err.code !== 'EISDIR' && err.code !== 'EPERM') reject(err);
-			resolve(moveDirAcrossDevice(source, dest, overwrite).catch(reject));
+			if (err.code !== 'EISDIR' && err.code !== 'EPERM') rej(err);
+			res(moveDirAcrossDevice(source, dest, overwrite).catch(rej));
 		});
 	});
 
 	outs.on('error', err => {
 		ins.destroy();
 		outs.destroy();
-		outs.removeListener('close', () => { resolve(fsn.unlink(source)); });
-		reject(err);
+		outs.removeListener('close', () => { res(fsn.unlink(source)); });
+		rej(err);
 	});
 
-	outs.once('close', () => { resolve(fsn.unlink(source)); });
+	outs.once('close', () => { res(fsn.unlink(source)); });
 	ins.pipe(outs);
 });
 
 const moveDirAcrossDevice = async (source, dest, overwrite) => {
 	const options = { overwrite: false };
-	if (overwrite) await fsn.remove(dest).catch(err => { throw err; });
-	await ncp(source, dest, options).catch(err => { throw err; });
+	if (overwrite) await fsn.remove(dest).catch(throwErr);
+	await ncp(source, dest, options).catch(throwErr);
 	return fsn.remove(source);
 };
 
@@ -278,107 +280,95 @@ const rmdir = async (myPath, options, originalEr) => fsn.rmdir(myPath).catch(er 
 });
 
 const rmkids = async(myPath, options) => {
-	const files = fsn.readdir(myPath).catch(er => { throw er; });
+	const files = fsn.readdir(myPath).catch(throwErr);
 	if (files.length === 0) return options.rmdir(myPath);
-	return Promise.all(files.map(file => fsn.remove(path.join(myPath, file), options)))
+	return Promise.all(files.map(file => fsn.remove(join(myPath, file), options)))
 		.then(() => fsn.rmdir(myPath));
 };
 
 const isWritable = (myPath) => fsn.lstat(myPath).then(() => false).catch(err => err.code === 'ENOENT');
 
 const ncp = async (source, dest, options = {}) => {
-	const basePath = process.cwd();
-	const currentPath = path.resolve(basePath, source);
-	const targetPath = path.resolve(basePath, dest);
+	options.basePath = process.cwd();
+	options.currentPath = resolve(options.basePath, source);
+	options.targetPath = resolve(options.basePath, dest);
+	options.filter = typeof options.filter === 'function' ? options.filter : undefined;
+	options.overwrite = options.overwrite ? options.clobber : true;
+	options.preserveTimestamps = options.preserveTimestamps === true;
+	return startCopy(options.currentPath, options);
+};
 
-	const filter = typeof options.filter === 'function' ? options.filter : undefined;
-	const transform = options.transform;
-	let overwrite = options.overwrite;
-	// If overwrite is undefined, use clobber, otherwise default to true:
-	if (overwrite === undefined) overwrite = options.clobber;
-	if (overwrite === undefined) overwrite = true;
-	const errorOnExist = options.errorOnExist;
-	const dereference = options.dereference;
-	const preserveTimestamps = options.preserveTimestamps === true;
-
-	const startCopy = async (mySource) => {
-		if (filter && !filter(mySource, dest)) return null;
-		return getStats(mySource);
+const startCopy = async (mySource, options) => {
+	if (options.filter && !options.filter(mySource, options.targetPath)) return null;
+	const stat = options.dereference ? fsn.stat : fsn.lstat;
+	const stats = await stat(mySource).catch(throwErr);
+	const item = {
+		name: mySource,
+		mode: stats.mode,
+		mtime: stats.mtime,
+		atime: stats.atime,
+		stats: stats
 	};
 
-	const getStats = async (mySource) => {
-		const stat = dereference ? fsn.stat : fsn.lstat;
-		const stats = await stat(mySource).catch(err => { throw err; });
-		const item = {
-			name: mySource,
-			mode: stats.mode,
-			mtime: stats.mtime,
-			atime: stats.atime,
-			stats: stats
-		};
+	if (stats.isDirectory()) {
+		const target = item.name.replace(options.currentPath, options.targetPath.replace('$', '$$$$'));
+		if (await isWritable(target)) return mkDir(item, target, options);
+		return copyDir(item.name);
+	} else if (stats.isFile() || stats.isCharacterDevice() || stats.isBlockDevice()) {
+		const target = item.name.replace(options.currentPath, options.targetPath.replace('$', '$$$$'));
+		if (await isWritable(target)) return copyFile(item, target, options);
+		else if (options.overwrite) return fsn.unlink(item).then(() => { copyFile(item, target, options); });
+		else if (options.errorOnExist) throw new Error(`${target} already exists`);
+	} else if (stats.isSymbolicLink()) {
+		const target = item.replace(options.currentPath, options.targetPath);
+		const resolvedPath = await fsn.readlink(item).catch(throwErr);
+		return checkLink(resolvedPath, target, options);
+	}
+	throw new Error('FS-NEXTRA: An Unkown error has occured in startCopy.');
+};
 
-		if (stats.isDirectory()) {
-			const target = item.name.replace(currentPath, targetPath.replace('$', '$$$$'));
-			if (await isWritable(target)) return mkDir(item, target);
-			return copyDir(item.name);
-		} else if (stats.isFile() || stats.isCharacterDevice() || stats.isBlockDevice()) {
-			const target = item.name.replace(currentPath, targetPath.replace('$', '$$$$'));
-			if (await isWritable(target)) return copyFile(item, target);
-			else if (overwrite) return fsn.unlink(item).then(() => { copyFile(item, target); });
-			else if (errorOnExist) throw new Error(`${target} already exists`);
-		} else if (stats.isSymbolicLink()) {
-			const target = item.replace(currentPath, targetPath);
-			const resolvedPath = await fsn.readlink(item).catch(err => { throw err; });
-			return checkLink(resolvedPath, target);
-		}
-		throw new Error('FS-NEXT: An Unkown error has occured in getStats.');
-	};
+const copyFile = (file, target, options) => new Promise((res, rej) => {
+	const readStream = fsn.createReadStream(file.name);
+	const writeStream = fsn.createWriteStream(target, { mode: file.mode });
 
-	const copyFile = (file, target) => new Promise((resolve, reject) => {
-		const readStream = fsn.createReadStream(file.name);
-		const writeStream = fsn.createWriteStream(target, { mode: file.mode });
+	readStream.on('error', rej);
+	writeStream.on('error', rej);
 
-		readStream.on('error', reject);
-		writeStream.on('error', reject);
+	if (options.transform) options.transform(readStream, writeStream, file);
+	else writeStream.on('open', () => { readStream.pipe(writeStream); });
 
-		if (transform) transform(readStream, writeStream, file);
-		else writeStream.on('open', () => { readStream.pipe(writeStream); });
-
-		writeStream.once('close', async() => {
-			const error = await fsn.chmod(target, file.mode).catch(err => err);
-			if (error) return reject(error);
-			if (!preserveTimestamps) return resolve();
-			const fd = await fsn.open(path, 'r+').catch(err => err);
-			if (fd instanceof Error) return reject(fd);
-			const futimesErr = fsn.futimes(fd, file.atime, file.mtime).catch(err => err);
-			const closeErr = fs.close(fd).catch(err => err);
-			if (futimesErr || closeErr) return reject(futimesErr || closeErr);
-			return resolve();
-		});
+	writeStream.once('close', async() => {
+		const error = await fsn.chmod(target, file.mode).catch(err => err);
+		if (error) return rej(error);
+		if (!options.preserveTimestamps) return res();
+		const fd = await fsn.open(target, 'r+').catch(err => err);
+		if (fd instanceof Error) return rej(fd);
+		const futimesErr = fsn.futimes(fd, file.atime, file.mtime).catch(err => err);
+		const closeErr = fs.close(fd).catch(err => err);
+		if (futimesErr || closeErr) return rej(futimesErr || closeErr);
+		return res();
 	});
+});
 
-	const mkDir = async (dir, target) => {
-		await fsn.mkdir(target, dir.mode).catch(err => { throw err; });
-		await fsn.chmod(target, dir.mode).catch(err => { throw err; });
-		return copyDir(dir.name);
-	};
+const mkDir = async (dir, target, options) => {
+	await fsn.mkdir(target, dir.mode).catch(throwErr);
+	await fsn.chmod(target, dir.mode).catch(throwErr);
+	return copyDir(dir.name, options);
+};
 
-	const copyDir = async (dir) => {
-		const items = await fsn.readdir(dir).catch(err => { throw err; });
-		return Promise.all(items.map(item => startCopy(path.join(dir, item))));
-	};
+const copyDir = async (dir, options) => {
+	const items = await fsn.readdir(dir).catch(throwErr);
+	return Promise.all(items.map(item => startCopy(join(dir, item), options)));
+};
 
-	const checkLink = async (resolvedPath, target) => {
-		if (dereference) resolvedPath = path.resolve(basePath, resolvedPath);
-		if (await isWritable(target)) return fsn.symlink(resolvedPath, target);
-		let targetDest = await fsn.readlink(target).catch(err => { throw err; });
-		if (dereference) targetDest = path.resolve(basePath, targetDest);
-		if (targetDest === resolvedPath) return null;
-		await fsn.unlink(target).catch(err => { throw err; });
-		return fsn.symlink(resolvedPath, target);
-	};
-
-	return startCopy(currentPath);
+const checkLink = async (resolvedPath, target, options) => {
+	if (options.dereference) resolvedPath = resolve(options.basePath, resolvedPath);
+	if (await isWritable(target)) return fsn.symlink(resolvedPath, target);
+	let targetDest = await fsn.readlink(target).catch(throwErr);
+	if (options.dereference) targetDest = resolve(options.basePath, targetDest);
+	if (targetDest === resolvedPath) return null;
+	await fsn.unlink(target).catch(throwErr);
+	return fsn.symlink(resolvedPath, target);
 };
 
 module.exports = fsn;
