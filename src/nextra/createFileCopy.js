6a6bd14782a656fed19430d6805cbe2641463c12
bdistin <bdistin@gmail.com>
2017-09-22 14:09:24 -0500

    minor fixes

--- src/nextra/createFileCopy.js
+++ src/nextra/createFileCopy.js
@@ -7,7 +7,17 @@ const mkdirs = require('./mkdirs');
 const pathExists = require('./pathExists');
 
 /**
- * Creates an empty file, making all folders required to satisfy the given file path.
+ * Creates an file copy, making all folders required to satisfy the given file path.
+ * @function ensureFileCopy
+ * @memberof fsn/nextra
+ * @param  {string} source The path to the file you want to copy
+ * @param  {string} destination The path to the file destination
+ * @param  {writeOptions|string} [options] The write options or the encoding string.
+ * @param  {boolean} [atomic = false] Whether the operation should run atomicly
+ * @return {Promise<void>}
+ */
+/**
+ * Creates an file copy, making all folders required to satisfy the given file path.
  * @function createFileCopy
  * @memberof fsn/nextra
  * @param  {string} source The path to the file you want to copy
