18dee78ef9be40d585e839cfb5b996e47c13eb4a
bdistin <bdistin@gmail.com>
2017-10-04 16:01:02 -0500

    start coverage and docs changes

---
+++ scripts/.travis-coverage.sh
@@ -0,0 +1,13 @@
+set -e
+
+if [ "$TRAVIS_BRANCH" != "master" -o -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo -e "Testing a non master branch push - testing without reporting."
+  npm run test:coverage
+  exit 0
+fi
+
+echo -e "Testing a master branch push - testing and reporting coverage."
+
+npm run test:coverage
+
+npm run coveralls
