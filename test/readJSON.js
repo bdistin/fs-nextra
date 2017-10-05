aca3d3bf81e979b3de1f8766b3949ba83ef9ed98
AoDude <aodude@runeinfo.xyz>
2017-10-05 11:56:11 -0500

    small fixes

--- test/readJSON.js
+++ test/readJSON.js
@@ -2,14 +2,14 @@ const ava = require('ava');
 const { fs, tempFileLoc } = require('./lib');
 const nextra = require('../src');
 
-ava.skip('standard usage', async test => {
+ava('standard usage', async test => {
 	const file = tempFileLoc();
 	fs.writeFileAsync(file, JSON.stringify({ validate: true }));
 	const readJSON = await nextra.readJSON(file);
 	test.true(readJSON.validate);
 });
 
-ava.skip('string options', async test => {
+ava('string options', async test => {
 	const file = tempFileLoc();
 	fs.writeFileAsync(file, JSON.stringify({ validate: true }));
 	const readJSON = await nextra.readJSON(file, 'utf8');
