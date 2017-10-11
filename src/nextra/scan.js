328d1316b9a4ce2bf9384e7b31823de1dc2fd42f
Unknown <bdistin@gmail.com>
2017-10-10 22:53:26 -0500

    impliment limit

--- src/nextra/scan.js
+++ src/nextra/scan.js
@@ -1,13 +1,14 @@
 const { resolve, join } = require('path');
 const { lstat, readdir } = require('../fs');
 
-module.exports = async function scan(root) {
-	return getStat(resolve(root), new Map());
+module.exports = async function scan(root, limit) {
+	return getStat(resolve(root), new Map(), -1, limit);
 };
 
-const getStat = async (dir, results) => {
+const getStat = async (dir, results, level, limit) => {
+	level++;
 	const stats = await lstat(dir);
 	results.set(dir, stats);
-	if (stats.isDirectory()) await Promise.all((await readdir(dir)).map(part => getStat(join(dir, part), results)));
+	if (stats.isDirectory() && (!limit || level < limit)) await Promise.all((await readdir(dir)).map(part => getStat(join(dir, part), results)));
 	return results;
 };
