ae69bc45e45acfa3e72534453a7d74c1ebfc44ba
bdistin <bdistin@gmail.com>
2017-10-10 23:06:15 -0500

    add filter

--- src/nextra/scan.js
+++ src/nextra/scan.js
@@ -1,13 +1,13 @@
 const { resolve, join } = require('path');
 const { lstat, readdir } = require('../fs');
 
-module.exports = async function scan(root, limit) {
-	return getStat(resolve(root), new Map(), -1, limit);
+module.exports = async function scan(root, options) {
+	return getStat(resolve(root), new Map(), -1, options);
 };
 
-const getStat = async (dir, results, level, limit) => {
+const getStat = async (dir, results, level, options) => {
 	const stats = await lstat(dir);
-	results.set(dir, stats);
-	if (stats.isDirectory() && (typeof limit === 'undefined' || level < limit)) await Promise.all((await readdir(dir)).map(part => getStat(join(dir, part), results, ++level, limit)));
+	if (!options.filter || options.filter(stats)) results.set(dir, stats);
+	if (stats.isDirectory() && (typeof options.limit === 'undefined' || level < options.limit)) await Promise.all((await readdir(dir)).map(part => getStat(join(dir, part), results, ++level, limit)));
 	return results;
 };
