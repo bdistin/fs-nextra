92a318ba13a2824db2d742d28e50b991b578b141
bdistin <bdistin@gmail.com>
2017-11-18 17:01:51 -0600

    Add matrix testing support

--- scripts/.travis-coverage.sh
+++ scripts/.travis-coverage.sh
@@ -7,6 +7,13 @@ if [ "$TRAVIS_BRANCH" != "master" -o -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" 
   exit 0
 fi
 
+# For Node != 8, do nothing
+if [ "$TRAVIS_NODE_VERSION" != "8" ]; then
+  echo -e "Build triggered with Node v${TRAVIS_NODE_VERSION} - testing without reporting."
+  npm run test:coverage
+  exit 0
+fi
+
 echo -e "Testing a master branch push - testing and reporting coverage."
 
 npm run test:coverage
