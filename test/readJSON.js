d44a4faf1e9157961f66caee97819142f62cdf2c
bdistin <bdistin@gmail.com>
2018-02-08 20:38:14 -0600

    fix broken test?

--- test/readJSON.js
+++ test/readJSON.js
@@ -4,14 +4,14 @@ const nextra = require('../src');
 
 ava('standard usage', async test => {
 	const file = tempFileLoc();
-	fs.writeFileAsync(file, JSON.stringify({ validate: true }));
+	await fs.writeFileAsync(file, JSON.stringify({ validate: true }));
 	const readJSON = await nextra.readJSON(file);
 	test.true(readJSON.validate);
 });
 
 ava('string options', async test => {
 	const file = tempFileLoc();
-	fs.writeFileAsync(file, JSON.stringify({ validate: true }));
+	await fs.writeFileAsync(file, JSON.stringify({ validate: true }));
 	const readJSON = await nextra.readJSON(file, 'utf8');
 	test.true(readJSON.validate);
 });
