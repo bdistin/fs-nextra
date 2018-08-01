2fddaa662ad01e4be08c1e1e0699997ef11e283f
bdistin <bdistin@gmail.com>
2018-08-01 08:43:20 -0500

    test: Add test for bug kyra found

--- test/emptyDir.js
+++ test/emptyDir.js
@@ -1,6 +1,14 @@
 const ava = require('ava');
-const { fs, tempFile, tempDir } = require('./lib');
+const { fs, tempFile, tempDir, tempDirLoc } = require('./lib');
 const nextra = require('../src');
+
+ava('nonexistent', async test => {
+	const dir = tempDirLoc();
+	await nextra.emptyDir(dir);
+
+	const contents = await fs.readdirAsync(dir);
+	test.true(contents.length === 0);
+});
 
 ava('empty', async test => {
 	const dir = tempDir();
