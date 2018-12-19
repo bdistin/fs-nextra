2c2e048224439bf5c1617fe101b4672e842bc2f0
Unknown <bdistin@gmail.com>
2018-12-19 14:18:57 -0600

    make prs report coverage too

--- scripts/.travis-coverage.sh
+++ scripts/.travis-coverage.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ "$TRAVIS_BRANCH" != "master" -o -n "$TRAVIS_TAG" -o "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo -e "Testing a non master branch push - testing without reporting."
-  npm run test:coverage
-  exit 0
-fi
-
 # For Node != 8, do nothing
 if [ "$TRAVIS_NODE_VERSION" != "8" ]; then
   echo -e "Build triggered with Node v${TRAVIS_NODE_VERSION} - testing without reporting."
@@ -14,7 +8,7 @@ if [ "$TRAVIS_NODE_VERSION" != "8" ]; then
   exit 0
 fi
 
-echo -e "Testing a master branch push - testing and reporting coverage."
+echo -e "Build triggered with Node v${TRAVIS_NODE_VERSION} - testing and reporting coverage."
 
 npm run test:coverage
 
