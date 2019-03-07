35e7bdc462af24f86852e2d1d12cc94a1be04fde
Unknown <bdistin@gmail.com>
2019-03-06 20:33:03 -0600

    ugh

--- scripts/.azure-coverage.sh
+++ scripts/.azure-coverage.sh
@@ -1,6 +1,6 @@
 set -e
 
-# For Node != 8, do nothing
+# For Node != 10.x or Platform != Linux, do nothing
 if [ "$NODE_VERSION" != "10.x" -o "$PLATFORM" != "Linux"]; then
   echo -e "Build triggered on ${PLATFORM} - Node v${NODE_VERSION} - testing without reporting."
   yarn run test:coverage
