1e53b1656ed0ce7d200c54b4033f04221578b0cd
Unknown <bdistin@gmail.com>
2019-03-12 14:09:23 -0500

    add more unit tests

--- test/targzAtomic.ts
+++ test/targzAtomic.ts
@@ -16,6 +16,21 @@ ava('File', async test => {
 	test.true(stats.isFile());
 });
 
+ava('Files', async test => {
+	test.plan(2);
+
+	const file1 = tempFileLoc();
+	await nextra.writeFile(file1, 'test1', 'utf8');
+	const file2 = tempFileLoc();
+	await nextra.writeFile(file2, 'test2', 'utf8');
+	const fileName = `${tempFileLoc()}.tar.gz`;
+	const retVal = await nextra.targzAtomic(fileName, [file1, file2]);
+	const stats = await fs.stat(fileName);
+
+	test.is(retVal, undefined);
+	test.true(stats.isFile());
+});
+
 ava('Directory', async test => {
 	test.plan(2);
 
